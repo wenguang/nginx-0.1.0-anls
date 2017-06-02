15be588081fac377e6affbaf960b017ed126caec
wenguang pan <wenguangpan@wenguangdeMacBook-Pro.local>
2017-06-02 13:50:28 +0800

    init

---
+++ nginx-0.1.0-anls/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  nginx-0.1.0-anls
+//
+//  Created by wenguang pan on 2017/6/2.
+//  Copyright © 2017年 wenguang pan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
